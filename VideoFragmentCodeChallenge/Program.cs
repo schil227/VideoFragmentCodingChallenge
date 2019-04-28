611239d8bf4decaafeb56a9583c42e0beef357f7
schil227 <schil227@morris.umn.edu>
2019-04-28 11:25:37 -0500

    Add project files.

---
+++ VideoFragmentCodeChallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VideoFragmentCodeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
